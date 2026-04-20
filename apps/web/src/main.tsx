32cbab57c15d94f3f045e49e829dcf6e9ff8d89d
danielmkongo <danielgbmokngo@gmail.com>
2026-04-20 11:53:16 +0300

    no ui debug stuff

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
 import { ErrorBoundary } from './components/ui/ErrorBoundary';
@@ -44,7 +43,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             }}
           />
         </BrowserRouter>
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ErrorBoundary>
   </React.StrictMode>
